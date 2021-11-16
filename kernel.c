6debb56f582832729cd3c05dd8d3e2e604b475cf
nabilcanan <61281770+nabilcanan@users.noreply.github.com>
2021-11-16 18:04:33 -0500

    Add files via upload

---
+++ kernel.c
@@ -0,0 +1,15 @@
+void main(){
+int startVidMem = 0xb800;
+int vidMemOffset = 0x0;
+int white = 0x7;
+char* letters = "Hello World\0";
+while(*letters != 0x0){
+putInMemory(startVidMem, vidMemOffset, *letters);
+vidMemOffset++;
+putInMemory(startVidMem, vidMemOffset, white);
+vidMemOffset++;
+letters++;
+
+}
+while(1);
+}
